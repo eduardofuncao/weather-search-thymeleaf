97b821d62aea19dd04771ef259b5549a63f7b4a5
Eduardo Função <eduardofuncao@hotmail.com>
2025-03-14 01:28:58 -0300

    display city name instead of id in result page

--- src/main/java/br/com/fiap/weather_search/controller/DTO/CityDTO.java
+++ src/main/java/br/com/fiap/weather_search/controller/DTO/CityDTO.java
@@ -1,15 +1,25 @@
 package br.com.fiap.weather_search.controller.DTO;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
+import org.aspectj.lang.annotation.DeclareMixin;
 
 @Data
 public class CityDTO {
     private long id;
     @NotNull(message = "City name is mandatory")
     private String name;
+
     @NotNull(message = "Latitude value is mandatory")
+    @DecimalMin(value = "-90.0", message = "Latitude must greater than -90")
+    @DecimalMax(value = "90.0", message = "Latitude must be less than 90")
     private Double latitude;
+
     @NotNull(message = "Longitude value is mandatory")
+    @DecimalMin(value = "-180.0", message = "Longitude must be greater than -180")
+    @DecimalMax(value = "180.0", message = "Longitude must be less than 180")
     private Double longitude;
 }
