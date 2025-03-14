783e3a45281fbb7a42bce20ac076faccd4356750
Eduardo Função <eduardofuncao@hotmail.com>
2025-03-14 00:41:01 -0300

    correct internationalization settings and properties files encoding

--- src/main/java/br/com/fiap/weather_search/config/InternationalizationConfig.java
+++ src/main/java/br/com/fiap/weather_search/config/InternationalizationConfig.java
@@ -5,17 +5,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @Configuration
-public class InternationalizationConfig {
+public class InternationalizationConfig implements WebMvcConfigurer {
 
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
         messageSource.setBasename("classpath:messages");
+        messageSource.setUseCodeAsDefaultMessage(true);
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
     }
@@ -25,5 +29,17 @@ public class InternationalizationConfig {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.US);
         return slr;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor interceptor = new LocaleChangeInterceptor();
+        interceptor.setParamName("lang"); // Parameter name to look for
+        return interceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
--- src/main/java/br/com/fiap/weather_search/controller/LoginController.java
+++ src/main/java/br/com/fiap/weather_search/controller/LoginController.java
@@ -1,7 +1,13 @@
 package br.com.fiap.weather_search.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
@@ -15,4 +21,5 @@ public class LoginController {
     public String home() {
         return "home";
     }
+
 }
